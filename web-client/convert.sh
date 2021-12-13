4ea228431917e2e9575090cf5d243218823b5865
BenjaminAlpert <benjamin@a0-0.com>
2021-12-12 22:32:25 -0500

    commit

--- web-client/convert.sh
+++ web-client/convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=
+
 #tensorflowjs_converter --input_format keras --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/MLP.h5 ./web-client/saved_models/graphs/MLP
 #tensorflowjs_converter --input_format keras --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/CNN.h5 ./web-client/saved_models/graphs/CNN
 
@@ -8,7 +10,7 @@
 
 
 tensorflowjs_converter --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/MLP ./web-client/saved_models/graphs/MLP
-tensorflowjs_converter --input_format keras --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/CNN ./web-client/saved_models/graphs/CNN
+tensorflowjs_converter --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/CNN ./web-client/saved_models/graphs/CNN
 
-tensorflowjs_converter ./saved_models/MLP ./web-client/saved_models/layers/MLP
-tensorflowjs_converter ./saved_models/CNN ./web-client/saved_models/layers/CNN
+#tensorflowjs_converter ./saved_models/MLP ./web-client/saved_models/layers/MLP
+#tensorflowjs_converter ./saved_models/CNN ./web-client/saved_models/layers/CNN
