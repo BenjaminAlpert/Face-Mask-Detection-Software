7ba54ff29e8cb0a4bd9c49b8d92257d363b37b21
BenjaminAlpert <benjamin@a0-0.com>
2021-12-12 20:06:59 -0500

    commit

---
+++ web-client/convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tensorflowjs_converter --input_format keras --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/MLP.h5 ./web-client/saved_models/graphs/MLP
+tensorflowjs_converter --input_format keras --output_format=tfjs_graph_model --control_flow_v2=True ./saved_models/CNN.h5 ./web-client/saved_models/graphs/CNN
+
+tensorflowjs_converter --input_format keras --control_flow_v2=True ./saved_models/CNN.h5 ./web-client/saved_models/layers/CNN
+tensorflowjs_converter --input_format keras --control_flow_v2=True ./saved_models/CNN.h5 ./web-client/saved_models/layers/CNN
