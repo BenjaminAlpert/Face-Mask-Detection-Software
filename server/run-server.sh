5522fb6d5c5b06215c1ef6deccf05fc48fc148bd
BenjaminAlpert <benjamin@a0-0.com>
2021-12-17 18:39:08 -0500

    commit

--- server/run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#docker run --gpus all -it -p 8888:8888 -v ~/workspace/academics-uvm-f21-cs254a-final-project/server/jupyter/:/root/.jupyter/ -v ~/workspace/academics-uvm-f21-cs254a-final-project:/tf tensorflow/tensorflow:latest-gpu-jupyter
-docker run --gpus all -it -p 8888:8888 -v ~/workspace/academics-uvm-f21-cs254a-final-project/server/jupyter/:/root/.jupyter/ -v ~/workspace/academics-uvm-f21-cs254a-final-project:/tf tensorflow/tensorflow:2.6.0-gpu-jupyter
