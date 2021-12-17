8b4de06e7765554ae463c7786643b8d6817570d0
BenjaminAlpert <benjamin@a0-0.com>
2021-12-17 17:41:19 -0500

    commit

--- web-client/index.js
+++ web-client/index.js
@@ -1,34 +1,63 @@
-const predict = async function(model, webcamElement){
-	image = tf.browser.fromPixels(webcamElement);
+const predict = async function(model, webcam){
+//	image = tf.browser.fromPixels(webcamElement);
+	image = await webcam.capture();
 	image = image.expandDims(0).toFloat();
 	const prediction = model.predict(image);
 	score = tf.softmax(prediction);
 	result = await score.argMax(-1).data();
 	confidence = await score.max().data();
 	out = {score:score, result:result[0], confidence:confidence[0]};
-	console.log(out);
 	return out;
 }
 
+const changeText = function(backgroundColor, textColor, text){
+	$("#text").css("background-color", backgroundColor,);
+        $("#text").css("color", textColor);
+        $("#text").html(text);
+}
 
-const predictAndChangeText = async function(model, webcamElement){
-	$("#text").css("background-color", "black");
-	$("#text").css("color", "white");
-	$("#text").text("Scanning...");
-	prediction = await predict(model, webcamElement);
-	while(prediction.confidence < 0.95){
-		prediction = await predict(model, webcamElement);
-	}
-	if(prediction.result == 0){
-		$("#text").css("background-color", "black");
-		$("#text").css("color", "green");
-		$("#text").text("Access Granted");
+const predictAndChangeTextRecursive = async function(model, webcam, prediction, counter){
+	console.log(prediction);
+	if(prediction.confidence >= 0.95){
+		document.getElementById('webcam').pause();
+		if(prediction.result == 0){
+			changeText("black", "green", "Access Granted");
+		}
+		else{
+			changeText("black", "red", "Access Denied");
+		}
 	}
 	else{
-		$("#text").css("background-color", "black");
-		$("#text").css("color", "red");
-		$("#text").text("Access Denied");
+		if(counter >= 45){
+                        changeText("black", "white", "Scanning...<br>Please Move Around to Get Better Images");
+                }
+		prediction = await predict(model, webcam);
+		predictAndChangeTextRecursive(model, webcam, prediction, counter+1);
 	}
+}
+
+const predictAndChangeText = async function(model, webcam){
+	changeText("black", "white", "Scanning...");
+	var prediction = await predict(model, webcam);
+	predictAndChangeTextRecursive(model, webcam, prediction, 1);
+	/*let counter = 1;
+	while(prediction.confidence < 0.95 && counter <= 30){
+		console.log(counter);
+		prediction = await predict(model, webcamElement);
+		console.log(counter);
+		console.log(prediction);
+		counter++;
+		if(counter >= 45){
+			changeText("black", "white", "Scanning...<br>Please Move Around to Get Better Images");
+		}
+	}
+	webcamElement.pause();
+	if(prediction.result == 0){
+		changeText("black", "green", "Access Granted");
+	}
+	else{
+		changeText("black", "red", "Access Denied");
+	}*/
 
 }
 
@@ -39,28 +68,23 @@ const start = async function(){
 	$("#text").text("Loading Webcam...");
 	webcamElement = document.getElementById('webcam');
 	webcam = await tf.data.webcam(webcamElement);
-	$("#text").text("");
-	const go = function(){
+	changeText("black", "white", "Tap or Click to Start Scanning");
+	const scan = function(){
 		if(webcamElement.paused){
 			webcamElement.play();
-			$("#text").css("background-color", "transparent");
-			$("#text").text("");
+			changeText("transparent", "black", "");
 		}
 		else{
-			//predict(model, webcamElement).then(changeText);
-			predictAndChangeText(model, webcamElement);
+			predictAndChangeText(model, webcam);
 		}
 	}
 
 	$(document).keydown(function(e){
 		if(e.which == 32){
-			go();
+			scan();
 		}
 	});
-	$(document).click(go);
-
+	$("video").on('click touchstart', scan);
 }
 
-$(document).ready(function(){
-	start();
-});
+$(document).ready(start);
